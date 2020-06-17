440dac09941f16e88415c9e1c5bc154693e605db
jasmin282220 <65880477+jasmin282220@users.noreply.github.com>
2020-06-16 21:14:49 -0400

    Add files via upload

---
+++ numeros primos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero positivo:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int i; int a = 0;
+
+            for (i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    a++;
+                    i = numero;
+                    i++;
+                }
+                if (a == 0)
+                {
+                    Console.WriteLine(numero + "si es un numero primo");
+                }
+                else
+                {
+                    Console.WriteLine(numero + "no es un  numero primo");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+        
